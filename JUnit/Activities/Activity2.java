ccd61ef829171658dd129f7bae3ccf1237b3f837
nishaburman0718rani <132873523+nishaburman0718rani@users.noreply.github.com>
2023-06-29 21:13:26 +0530

    Add files via upload
    Activity2

---
+++ JUnit/Activities/Activity2.java
@@ -0,0 +1,29 @@
+package activity2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Activity2 {
+
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(BankAccount.NotEnoughFundsException.class, () -> account.withdraw(10),
+                "Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
---
+++ JUnit/Activities/BankAccount.java
@@ -0,0 +1,30 @@
+package activity2;
+
+public class BankAccount {
+
+    private Integer balance;
+
+    // Create a constructor
+    public BankAccount(Integer initialBalance) {
+        balance = initialBalance;
+    }
+
+    // Add method to calculate
+    // balance amount after withdrawal
+    public Integer withdraw(Integer amount) {
+        if (balance < amount) {
+            throw new NotEnoughFundsException(amount, balance);
+        }
+        balance -= amount;
+        return balance;
+    }
+
+
+    public class NotEnoughFundsException extends RuntimeException {
+
+        public NotEnoughFundsException(Integer amount, Integer balance) {
+            super("Attempted to withdraw " + amount + " with a balance of " + balance);
+        }
+
+    }
+}
