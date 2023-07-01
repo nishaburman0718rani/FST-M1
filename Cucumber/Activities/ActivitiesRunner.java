0cac70e846ab1c986f896cff3abecea3335f0243
nishaburman0718rani <132873523+nishaburman0718rani@users.noreply.github.com>
2023-07-01 13:25:52 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -0,0 +1,19 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src\\test\\java\\Features",
+        glue = {"stepDefinitions"},
+        tags = "@SimpleAlert",
+        plugin = {"pretty"},
+        //plugin = {"html: test-reports"},
+        // plugin = {"json: test-reports/json-report.json"},
+        monochrome = true
+)
+
+public class ActivitiesRunner {
+}
